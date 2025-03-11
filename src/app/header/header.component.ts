c86620c530fa158c68043b0dd52f97e60b484a80
oscarrep <oscar.bcn.95@gmail.com>
2025-03-11 13:31:03 +0100

    cleaned comments + reposition header buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,8 @@
 import { Component, Input, inject } from '@angular/core';
-import { StarshipsComponent } from "../starships/starships.component";
-import { HomeComponent } from "../home/home.component";
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  //imports: [StarshipsComponent, HomeComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
