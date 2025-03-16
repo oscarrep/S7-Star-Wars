0de1afd3b09d701a1ee76b456c264e8b13aec6b5
oscarrep <oscar.bcn.95@gmail.com>
2025-03-16 13:53:48 +0100

    created components to show ship pilots and their films

---
+++ src/app/films/films/films.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-films',
+  imports: [],
+  templateUrl: './films.component.html',
+  styleUrl: './films.component.scss'
+})
+export class FilmsComponent {
+
+}
---
+++ src/app/pilots/pilots/pilots.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pilots',
+  imports: [],
+  templateUrl: './pilots.component.html',
+  styleUrl: './pilots.component.scss'
+})
+export class PilotsComponent {
+
+}
