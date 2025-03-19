b2cd29a0d6c5ad48a6b11b411c6b30f1b0a64b71
oscarrep <oscar.bcn.95@gmail.com>
2025-03-19 18:49:35 +0100

    vercel-build

---
+++ vercel-build.sh
@@ -0,0 +1,16 @@
+echo "Creating environment.ts..."
+ 
+ cat <<EOT > src/environments/environment.ts
+ export const environment = {
+   production: true,
+   firebaseConfig: {
+     apiKey: '${FIREBASE_API_KEY}',
+     authDomain: '${FIREBASE_AUTH_DOMAIN}',
+     projectId: '${FIREBASE_PROJECT_ID}',
+     storageBucket: '${FIREBASE_STORAGE_BUCKET}',
+     messagingSenderId: '${FIREBASE_MESSAGING_SENDER_ID}',
+     appId: '${FIREBASE_APP_ID}'
+     measurementId: '${FIREBASE_MEASUREMENT_ID}'
+   }
+ };
+ EOT
